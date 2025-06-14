1abd1c136c374c257c5b26e9879297214c44854b
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-06-15 00:25:39 +0600

    animesuge scrapping

--- anime.js
+++ anime.js
@@ -2,12 +2,12 @@ import * as cheerio from 'cheerio';
 import axios from 'axios';
 
 export const Anitaku = async () => {
-    const resq = await axios.get("https://anitaku.io/");
+    const res_pages = await axios.get("https://animesugetv.se/genre/action?page=2");
     //    console.log(resq.data);
     
-    const $ = cheerio.load(resq.data);
-    $('.listupd > .excstf > article').each((i, dat) => {
-        const aTag = $(dat).find('div > a');
+    const $ = cheerio.load(res_pages.data);
+    $('.border-secondary > .main-card > div').each((i, dat) => {
+        const aTag = $(dat).find('div > .inner > .item-top > a');
         const link = aTag.attr('href');
         if (link) {
             console.log(link);
