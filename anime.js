270fe0b99bd9b39886fea4729a516de6dc7b7c5a
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-06-19 00:35:52 +0600

    mins sub ep tV added

--- anime.js
+++ anime.js
@@ -10,10 +10,17 @@ export const AniHQ = async (page = 1) => {
         const aTag = $(dat).find('.kira-anime > a');
         const page_links = aTag.attr('href');
         const title = $(dat).find('span[data-en-title]').text().trim();
+        const title_show = $(dat).find('.show').text().trim();
+        const Television = $(dat).find('.uppercase').text().trim();
+        const minText = $(dat).find('.text-xs.text-text-color.w-full.line-clamp-1').text();
+        const minMatch = minText.match(/(\d+\s*(?:m|mins|minutes))/i);
+        const min = minMatch ? minMatch[0].replace(/\s+/g, '') : null;
+        const sub = $(dat).find('.mie-px').text().trim();
+        const episode = $(dat).find('.plb-1').text().trim();
         const imgTag = $(dat).find('.kira-anime img');
         const img = imgTag.attr('src');
-        if (page_links && img && title) {
-            results.push({ page_links, img, title });
+        if (page_links && img && title && title_show && Television && min && episode && sub) {
+            results.push({ page_links, img, title, title_show, Television, min, episode, sub });
         }
     });
 
